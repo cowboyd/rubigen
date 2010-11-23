40cd9dbd197a6e2caf1e23239a31269ac1c42896
Charles Lowell <cowboyd@thefrontside.net>
2010-11-23 11:55:26 -0600

    vendor just enough bits of bits of activesupport-3.0.3 and i18n-0.4.2 so that the generation cukes pass.

--- lib/rubigen.rb
+++ lib/rubigen.rb
@@ -1,13 +1,10 @@
 $:.unshift(File.dirname(__FILE__)) unless
   $:.include?(File.dirname(__FILE__)) || $:.include?(File.expand_path(File.dirname(__FILE__)))
 
-begin
-  # if you are using rubygems, fix to 2.3.5
-  gem 'activesupport', '~> 2.3.5'
-rescue
-end
-
-require 'active_support' 
+require 'active_support/core_ext/class/inheritable_attributes'
+require 'active_support/core_ext/class/attribute_accessors'
+require 'active_support/core_ext/object/blank'
+require 'active_support/core_ext/string/inflections'
 
 module RubiGen
   VERSION = '1.5.5'
